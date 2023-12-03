e7d2ab8156b6457b23633cd1239ca52ce9f3ab2d
rachelhoneycutt <142539235+rachelhoneycutt@users.noreply.github.com>
2023-12-02 23:32:15 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,30 @@ $(function(){
     
     var year = "";
     var numClick = 0;
+    var time = "";
 
-    $('.btn').click(function(){
+    $('.year').click(function(){
       $('#console-box').prepend('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
       year += $(this).text();  
       numClick++;
       if (numClick == 4) {
         $('#console-box').prepend('<br>Year Selected:'+year);
+        numClick = 0;
       }
     });
+
+    $('.time').click(function(){
+      time += $(this).text();  
+      numClick++;
+      if (numClick == 2) {
+        time += ":"
+      }
+      if (numClick == 4) {
+        $('#output').html('<h1>Time Selected ' + time + ' ' +$('#time').val() + '</h1>');
+        numClick = 0;
+        time = "";
+      }
+    });
+    
   });
   
